3c89997dcb4aa36f1e22cff0ecc873c4f66dd4b5
ngvanlinh3103 <67747255+ngvanlinh3103@users.noreply.github.com>
2022-06-24 13:55:39 +0700

    update time

--- game.js
+++ game.js
@@ -1,28 +1,29 @@
-var so_pt=prompt('nhap gia tri');
+var so_pt = prompt('nhap gia tri');
 var html_game = "";
-var main_game = document.getElementsByClassName('game');
+var main_game = document.getElementById('game');
 
 //in ra mang
-for (let i=0;i<so_pt;i++){
-    for (let j=0; j<so_pt;j++){
-        document.write('<button class="btn" id="'+i,j+'"></button>');
+for (let i = 0; i < so_pt; i++) {
+    for (let j = 0; j < so_pt; j++) {
+        html_game += '<button class="btn" id="' + i + '' + j + '"></button>';
     }
-    document.write('<br>');
+    html_game += '<br>';
 }
+main_game.innerHTML = html_game;
 
 // tim id ben canh id dc click
 function change(ab) {
-    let ad= ab.toString();
-    let e=ad.match(/[0-9]/)[0];
-    let f=ad.match(/[0-9]$/)[0];
-    let a=Number(e);
-    let b=Number(f);
+    let ad = ab.toString();
+    let e = ad.match(/[0-9]/)[0];
+    let f = ad.match(/[0-9]$/)[0];
+    let a = Number(e);
+    let b = Number(f);
     /*console.log('a:'+a+' b:'+b);*/
     var d = [];
     for (var i = (a - 1); i <= (a + 1); i++) {
-        if (i >= 0 && i<so_pt) {
+        if (i >= 0 && i < so_pt) {
             for (var j = (b - 1); j <= (b + 1); j++) {
-                if (j >= 0 && j<so_pt) {
+                if (j >= 0 && j < so_pt) {
                     let x = "";
                     x = i.toString() + j.toString();
                     if (x != ab) {
@@ -36,36 +37,71 @@ function change(ab) {
     return d;
 }
 
-function check(){
-    var ele=document.getElementsByClassName('btn');
+function check() {
+    var ele = document.getElementsByClassName('btn');
     for (let i = 0; i < ele.length; i++) {
-        if(ele[i].classList.length < 2){
+        if (ele[i].classList.length < 2) {
             return false;
         }
     }
     return true;
 }
-var elements= document.getElementsByClassName('btn');
-for (let i=0; i<elements.length;i++){
-    elements[i].addEventListener('click',function (){
+
+var elements = document.getElementsByClassName('btn');
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function () {
         //console.log(change(elements[i].id));
-        var n=this.id;
-        var changes=change(n);
+        var n = this.id;
+        var changes = change(n);
         for (let j = 0; j < changes.length; j++) {
-            if(document.getElementById(changes[j]).classList.length < 2 ){
+            if (document.getElementById(changes[j]).classList.length < 2) {
                 document.getElementById(changes[j]).classList.add('btn1');
-            }
-            else {
+            } else {
                 document.getElementById(changes[j]).classList.remove('btn1');
 
             }
 
         }
-        setTimeout(function (){
-            if (check()){
-                alert('win');
+        setTimeout(function () {
+            if (check()) {
+                if (confirm('win :)) thời gian: ' + document.getElementById('seconds').innerText + ':' + document.getElementById('tens').innerText)) {
+                    location.reload();
+                }
             }
-        }, 1000);
+        }, 50);
 
     })
+}
+
+//time
+window.onload = function () {
+    let second = 0;
+    let ten = 0;
+    var appendTens = document.getElementById('tens');
+    let appendSeconds = document.getElementById('seconds');
+    let btn_start = document.getElementById('start');
+    let interval;
+    interval = setInterval(star, 10);
+
+    function star() {
+        ten++;
+        if (ten < 10) {
+            appendTens.innerHTML = "0" + ten;
+        }
+        if (ten > 9) {
+            appendTens.innerHTML = ten;
+        }
+        if (ten > 99) {
+            second++;
+            ten = 0;
+            appendSeconds.innerHTML = "0" + second;
+            appendTens.innerHTML = "0" + ten;
+        }
+        if (second > 9) {
+            appendSeconds.innerHTML = second;
+        }
+        if (second > 59) {
+
+        }
+    }
 }
